7f98e72e57c2171c7ff7a8c565072819b5c8cba6
shalva97 <shalva258@gmail.com>
2026-03-31 19:48:20 +0400

    fix crashes

--- app/src/main/java/com/portal/browserbar/PortalApplication.kt
+++ app/src/main/java/com/portal/browserbar/PortalApplication.kt
@@ -5,36 +5,22 @@ import androidx.room.Room
 import com.portal.browserbar.data.local.AppDao
 import com.portal.browserbar.data.local.AppDatabase
 import com.portal.browserbar.data.repository.AppRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 
 class PortalApplication : Application() {
 
-    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+    val database: AppDatabase by lazy {
+        Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "portal_db"
+        ).fallbackToDestructiveMigration(true).build()
+    }
 
-    lateinit var database: AppDatabase
-        private set
+    val appDao: AppDao by lazy {
+        database.appDao()
+    }
 
-    lateinit var appDao: AppDao
-        private set
-
-    lateinit var repository: AppRepository
-        private set
-
-    override fun onCreate() {
-        super.onCreate()
-
-        applicationScope.launch(Dispatchers.IO) {
-            database = Room.databaseBuilder(
-                this@PortalApplication,
-                AppDatabase::class.java,
-                "portal_db"
-            ).fallbackToDestructiveMigration().build()
-
-            appDao = database.appDao()
-            repository = AppRepository(appDao, this@PortalApplication)
-        }
+    val repository: AppRepository by lazy {
+        AppRepository(appDao, this)
     }
 }
