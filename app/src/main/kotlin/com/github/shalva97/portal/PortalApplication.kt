f37ae93c538cf61483df9ad68a6a4dd3c9a53987
Giorgi Shalvashvili <george0shalvashvili@gmail.com>
2026-07-07 22:36:45 +0400

    fix uninstalled apps not disappearing

--- app/src/main/kotlin/com/github/shalva97/portal/PortalApplication.kt
+++ app/src/main/kotlin/com/github/shalva97/portal/PortalApplication.kt
@@ -1,9 +1,12 @@
 package com.github.shalva97.portal
 
 import android.app.Application
+import android.content.Intent
+import android.content.IntentFilter
 import com.github.shalva97.portal.data.local.AppStorage
 import com.github.shalva97.portal.data.local.IconStorage
 import com.github.shalva97.portal.data.repository.AppRepository
+import com.github.shalva97.portal.receiver.PackageChangeReceiver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -23,6 +26,16 @@ class PortalApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        registerReceiver(
+            PackageChangeReceiver(),
+            IntentFilter().apply {
+                addAction(Intent.ACTION_PACKAGE_ADDED)
+                addAction(Intent.ACTION_PACKAGE_REMOVED)
+                addAction(Intent.ACTION_PACKAGE_CHANGED)
+                addAction(Intent.ACTION_PACKAGE_REPLACED)
+                addDataScheme("package")
+            }
+        )
         applicationScope.launch(Dispatchers.IO) {
             if (!storage.isInitialRefreshDone()) {
                 repository.refreshApps()
